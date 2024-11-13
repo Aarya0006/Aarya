3f55fbc1c6d26908644c97a4fdaf4689cbfd7c11
Aarya0006 <aarya.22b0131107@abes.ac.in>
2024-11-13 13:14:16 +0530

    React First

---
+++ Day11/react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return <div id="disp"><h1>Student Details</h1>
+  <h2>Name: AArya Singh</h2>
+  <h2>Email Id: aarya@gmail.com</h2>
+  <h2>Roll No: 123456</h2>
+  </div>;
+};
+
+export default App;
